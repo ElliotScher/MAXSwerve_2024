6a59d194644aa4c1180d78aa99ef8cefbc036ad1
Elliot Scher <ecscher84@gmail.com>
2023-01-31 10:06:12 -0500

    gyro change

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -102,6 +102,7 @@ public class DriveSubsystem extends SubsystemBase {
             m_RightLeader.getEncoder().getPosition()
         );
         resetEncoders();
+        resetGyro();
     }
 
     @Override
